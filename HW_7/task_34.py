3b2f952e4500d700c36ac8a082e1cf73f03e8e2f
Ivan Sofil <ivan.sofilkanich.99@mail.ru>
2023-05-21 20:37:04 +0300

    Solved tasks

---
+++ HW_7/task_34.py
@@ -0,0 +1,17 @@
+input_text = input("Write your song sounds: ")
+check_letters = list('аеёиоуыэюя')
+lett_list = []
+# input_text.split(" ")
+
+
+for each in input_text.split(" "):
+    filtered = filter(lambda x: x in check_letters, each)
+    let_amount = len(list(map(lambda x: x, filtered)))
+    lett_list.append(let_amount)
+
+result = list(map(lambda x: x == lett_list[0], lett_list))
+
+if all(result) == True:
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
